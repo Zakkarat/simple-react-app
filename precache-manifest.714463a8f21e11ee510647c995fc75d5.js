44da661974a584250fdeca1370b9dff15cd95607
Zakkaray <bloomzakk@gmail.com>
2019-07-09 23:14:09 +0300

    Updates

---
+++ precache-manifest.714463a8f21e11ee510647c995fc75d5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7016bf5312a26de0742a956d42ac19e7",
+    "url": "/react-friends-app/index.html"
+  },
+  {
+    "revision": "763dfd458d29779a02c9",
+    "url": "/react-friends-app/static/css/main.11f2e55d.chunk.css"
+  },
+  {
+    "revision": "db218b4285b618d9a923",
+    "url": "/react-friends-app/static/js/2.715286a2.chunk.js"
+  },
+  {
+    "revision": "763dfd458d29779a02c9",
+    "url": "/react-friends-app/static/js/main.00d63020.chunk.js"
+  },
+  {
+    "revision": "68c65057ea4845e38c04",
+    "url": "/react-friends-app/static/js/runtime~main.837fcf4c.js"
+  }
+]);
